e91739fab7f2a464c6bcdfbb0eecc3b6f2ffb247
Hamza Ameen <ameenhamza392@gmail.com>
2025-06-18 18:38:52 +0500

    feat:push latest code

--- max-machinery-backend/src/config/database.config.ts
+++ max-machinery-backend/src/config/database.config.ts
@@ -4,8 +4,8 @@ export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT) || 5432,
-username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '12345678',
+username: process.env.DB_USERNAME || 'hamza123',
+  password: process.env.DB_PASSWORD || 'hamza123',
   database: process.env.DB_NAME || 'max-machinery-automation',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production', // Only enable in development
