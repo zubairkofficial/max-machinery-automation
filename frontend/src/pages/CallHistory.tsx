4ec2516b3199c06ad55888dedce28488f7664aa7
Hamza Ameen <ameenhamza392@gmail.com>
2025-07-07 20:55:24 +0500

    feat:push latest code

--- frontend/src/pages/CallHistory.tsx
+++ frontend/src/pages/CallHistory.tsx
@@ -40,6 +40,7 @@ interface CallDetail {
     company: string;
     industry: string;
     linkClicked: boolean;
+    linkSend: boolean;
     formSubmitted
     : boolean;
   };
@@ -448,7 +449,7 @@ const CallHistory: React.FC<CallHistoryPageProps> = () => {
                           <p className="text-gray-600 dark:text-gray-400">ReSchedule:</p>
                           <p className="font-medium text-gray-900 dark:text-white">
                          {
-    (selectedCall.lead.linkClicked || selectedCall.lead.formSubmitted || selectedCall.lead.contacted) 
+    (selectedCall.lead.linkClicked || selectedCall.lead.formSubmitted || selectedCall.lead.linkSend) 
       ? 'No Schedule' 
       : (selectedCall.lead.scheduledCallbackDate 
           ? new Date(selectedCall.lead.scheduledCallbackDate).toLocaleString() 
