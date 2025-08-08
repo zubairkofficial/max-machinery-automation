a055ada0e7381adedf554bc12decb83bf6482e45
Hamza Ameen <ameenhamza392@gmail.com>
2025-08-08 18:31:33 +0500

    feat:push latest code

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -4,7 +4,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT) || 5432,
-username: process.env.DB_USERNAME || 'postgres',
+username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '12345678',
   database: process.env.DB_NAME || 'max-machinery-automation',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
